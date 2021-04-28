ecfd39af761e7d39b14d630eff55b49ba836bd18
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-04-28 13:53:22 -0300

    add ex 11

--- exercises/tempCodeRunnerFile.js
+++ exercises/tempCodeRunnerFile.js
@@ -1,10 +1,37 @@
-let custo = 2;
-let venda = 0;
-let custoTotal = ((custo * 0.2) + custo);
-let lucro = (venda - custoTotal);
+let salarioBruto = 3500.10;
+let inss;
+let ir;
 
-if (custo > 0 && venda > 0) {
-    console.log(lucro * 100);
-} else {
-    console.log('erro')
+if (salarioBruto <= 1556.94) {
+    inss = salarioBruto * 0.08;
+} 
+else if (salarioBruto <= 2594.92) {
+    inss = salarioBruto * 0.09;
 }
+else if (salarioBruto <= 5189.82) {
+    inss = salarioBruto * 0.11;
+}
+else {
+    inss = salarioBruto - 570.88;
+};
+
+let salarioSemInss = salarioBruto - inss;
+
+if (salarioSemInss < 1903.99) {
+    ir = salarioSemInss;
+}
+else if (salarioSemInss < 2826.66) {
+    ir = (salarioSemInss * 0.075) - 142.80;
+}
+else if (salarioSemInss < 3751.06) {
+    ir = (salarioSemInss * 0.15) - 354.80;
+}
+else if (salarioSemInss < 4664.69) {
+    ir = (salarioSemInss * 0.225) - 636.13;
+}
+else {
+    ir = (salarioSemInss * 0.275) - 869.36;
+}
+
+
+console.log('O salário liquído é de ' + (salarioSemInss - ir) + ' reais.');
