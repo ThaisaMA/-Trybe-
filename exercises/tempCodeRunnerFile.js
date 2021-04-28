89644b7d8e25c412ddcc00c59b348b82016eace9
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-04-28 11:02:52 -0300

    add ex 8

--- exercises/tempCodeRunnerFile.js
+++ exercises/tempCodeRunnerFile.js
@@ -1,23 +1,9 @@
-let nota = -10;
-if (nota >= 90 && nota <= 100) {
-    console.log('A');
-}
-else if (nota >= 80 && nota < 90) {
-    console.log('B');
-}
-else if (nota >= 70 && nota < 80) {
-    console.log('C');
-}
-else if (nota >= 60 && nota < 70) {
-    console.log('D');  
-}
-else if (nota >= 50 && nota < 60) {
-    console.log('E');
-}
-else if (nota >= 0 && nota < 50) {
-    console.log('F');
-}
-else {
-    console.log('erro')
-}
+let num1 = 661;
+let num2 = 657;
+let num3 = 576;
 
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+    console.log('true');
+} else {
+    console.log('false');
+}
