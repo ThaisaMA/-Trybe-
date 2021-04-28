cb3b23b17b77cd1889fc263263d2237a849ac03f
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-04-28 11:10:24 -0300

    add ex 9

--- exercises/tempCodeRunnerFile.js
+++ exercises/tempCodeRunnerFile.js
@@ -1,8 +1,8 @@
-let num1 = 661;
-let num2 = 657;
+let num1 = 660;
+let num2 = 658;
 let num3 = 576;
 
-if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
     console.log('true');
 } else {
     console.log('false');
