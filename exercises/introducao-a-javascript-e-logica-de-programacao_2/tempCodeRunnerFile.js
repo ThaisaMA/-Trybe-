6f26c627e27bbc2fd90b1083b644fd0fd258bb37
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-04-29 23:02:49 -0300

    ex 1 e 2

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/tempCodeRunnerFile.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/tempCodeRunnerFile.js
@@ -1,5 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+let resultado = 0;
 for(let i = 0; i < numbers.length; i += 1) {
-    console.log(numbers[i]);
+    resultado = resultado + numbers[i];
 }
+console.log(resultado);
