d5b0b9151ccda2b6236e5c84090f4ed755ffe7c6
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-04-29 23:52:17 -0300

    ex 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/tempCodeRunnerFile.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/tempCodeRunnerFile.js
@@ -1,11 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
+let num = numbers[0];
 for(i = 0; i < numbers.length; i += 1) {
-    resultado = (resultado + numbers[i]); 
+    if (num < numbers[i - 1]) {
+    num = numbers[i - 1];
+    } else {
+        //num = numbers[i];
+    }
 }
-console.log(resultado / numbers.length);
-if ((resultado / numbers.length) > 20) {
-    console.log('valor maior que 20');
-} else {
-    console.log('valor menor ou igual a 20');
-}
+console.log(num);
